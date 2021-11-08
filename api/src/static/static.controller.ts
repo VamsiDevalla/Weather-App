a4d7e864ad8d80665fa8b4f396d8dfa807a76ccb
vamsidevalla <vammichanti40312@gmail.com>
2021-11-08 06:04:10 -0500

    removing static controlller form api

--- api/src/static/static.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendHtml } from './static.handler';
-
-const StaticController: Router = Router();
-
-StaticController.get('/', sendHtml);
-
-export default StaticController;
--- api/src/static/static.handler.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-
-export const sendHtml = async (request: Request, response: Response) => {
-  response.status(200).json({ data: 'place holder end point for SSR' });
-};
